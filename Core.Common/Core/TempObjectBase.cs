069760a0b0b35262abcc4c2f777ed63286de3589
nicwolford <nicwolford@gmail.com>
2015-08-17 16:53:09 -0400

    more stuff

--- Core.Common/Core/TempObjectBase.cs
+++ Core.Common/Core/TempObjectBase.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Common.Contracts
+namespace Core.Common.Core
 {
-    interface IAccountOwnedEntity
+    class TempObjectBase
     {
-        
     }
 }
