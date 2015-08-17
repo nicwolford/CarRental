840c83105529e85c7a6de1b033863aa0b9dd433c
nicwolford <nicwolford@gmail.com>
2015-08-17 13:30:26 -0400

    Initial Commit

---
+++ CarRental.Business.Entities/Rental.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Core;
+using Core.Common.Contracts;
+using System.Runtime.Serialization;
+
+namespace CarRental.Business.Entities
+{
+    [DataContract]
+    public class Rental : EntityBase, IIdentifiableEntity
+    {
+        [DataMember]
+        public int RentalId { get; set; }
+
+        [DataMember]
+        public int AccountId { get; set; }
+
+        [DataMember]
+        public int CarId { get; set; }
+
+        [DataMember]
+        public DateTime DateRented { get; set; }
+
+        [DataMember]
+        public DateTime DateDue { get; set; }
+
+        [DataMember]
+        public DateTime? DateReturned { get; set; }
+
+        public int EntityId
+        {
+            get
+            {
+                return RentalId;
+            }
+
+            set
+            {
+                RentalId = value;
+            }
+        }
+    }
+}
